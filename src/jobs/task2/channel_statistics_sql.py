2b4ea3d7748180a1a2ca9a1d11a3a56d595f3da3
Vladislav Vlasov <vvlasov@griddynamics.com>
2021-03-11 13:18:40 +0400

    Setup project structure. Initialize jobs structure

---
+++ src/jobs/task2/channel_statistics_sql.py
@@ -0,0 +1,63 @@
+"""
+This Python module contains a job which builds purchases attribution 
+projection.
+
+The job flow:
+  * ...
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+from pyspark.sql import SparkSession, DataFrame
+
+
+def main() -> None:
+    """
+    The job definition.
+
+    :return: None
+    """
+    pass
+
+
+def extract_data(ss: SparkSession) -> DataFrame:
+    """
+    Loads data from Parquet files.
+
+    :param ss: an instance of SparkSession.
+    :return: Spark DataFrame with input data.
+    """
+    pass
+
+
+def transform_data(df: DataFrame) -> DataFrame:
+    """
+    Tranforms original dataset.
+
+    :param df: dataset to transform as DataFrame.
+    :return: transformed DataFrame.
+    """
+    pass
+
+
+def load_data(df: DataFrame) -> None:
+    """
+    Collects data and writes results to Parquet files.
+
+    :param df: DataFrame to process.
+    :return: None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
