73d300a052963ce3a152fae52bbb11a9431404cd
v-vlasov <78155496+v-vlasov@users.noreply.github.com>
2021-03-16 12:26:19 +0400

    [Development] Add jobs configuration via YAML files (#6)

--- src/shared/spark.py
+++ src/shared/spark.py
@@ -1,15 +1,22 @@
 """
 This module contains helper function to initialize SparkSession object.
 """
-import os
+from typing import List
 
+import os
+import yaml
+
+from pyspark import SparkFiles
 from pyspark.sql import SparkSession
 
 import __main__
 from src.shared.logging import Log4jWrapper
 
 
-def start_spark_session(app_name: str) -> tuple:
+CONFIG_FILE_SUFFIX = "_config.yaml"
+
+
+def start_spark_session(app_name: str, files: List[str] = None) -> tuple:
     """
     Starts Spark session and initialize Spark Log4j logger wrapper.
     Returns session and logger objects as tuple.
@@ -23,9 +30,20 @@ def start_spark_session(app_name: str) -> tuple:
     interactive console session or from an environment which has a `DEBUG`
     environment variable set as '1' (e.g. by default in Visual Studio Code).
 
+    The function looks for a files ending with '_config.yaml' that can be sent
+    with the Spark job. If it is found, it is opened, the contents parsed
+    (assuming it contains valid YAML) into a dict of ETL job configuration
+    parameters, which are returned as the last element in the tuple
+    returned by this function. If the file cannot be found then the return
+    tuple only contains the Spark session and Spark logger objects
+    and None for config.
+
     :param app_name: name of Spark application.
-    :return: a tuple with Spark session and Spark logger wrapper objects.
+    :param files: list of files to send to Spark cluster (master and workers).
+    :return: a tuple with Spark session and Spark logger wrapper objects and
+             config dict if presented.
     """
+    files = files or []
     spark_builder: SparkSession.Builder = SparkSession.builder.appName(app_name)
 
     # Detect execution environment type
@@ -33,8 +51,28 @@ def start_spark_session(app_name: str) -> tuple:
         # Determined as local, add more parameters
         spark_builder.master("local[*]")
 
+        # Add additional files
+        spark_builder.config('spark.files', ','.join(files))
+
     # Create session and logger objects
     spark_session: SparkSession = spark_builder.getOrCreate()
     logger: Log4jWrapper = Log4jWrapper(spark_session.sparkContext)
 
-    return (spark_session, logger)
+    # Get configuration from config files
+    # get config file if sent to cluster with --files
+    spark_files_dir = SparkFiles.getRootDirectory()
+    config_files = [filename for filename in os.listdir(spark_files_dir)
+                    if filename.endswith(CONFIG_FILE_SUFFIX)]
+
+    if config_files:
+        config_dict: dict = {}
+        for cfg_file in config_files:
+            path_to_config_file = os.path.join(spark_files_dir, cfg_file)
+            with open(path_to_config_file, 'r') as config_file:
+                config_dict = {**config_dict, **yaml.safe_load(config_file)}
+            logger.info(f"parsed config file: {cfg_file}")
+    else:
+        logger.info('no config files found')
+        config_dict = None
+
+    return (spark_session, logger, config_dict)
